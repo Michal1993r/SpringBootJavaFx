d7689f1e6b175159e658d1698101a892abd9c92e
Michał Celniak <michal.celniak@hatomi.pl>
2016-11-03 11:17:20 +0100

    integration

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,7 +1,39 @@
 package sample;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Component
+@org.springframework.stereotype.Controller
 public class Controller {
+
+	@FXML
+	private Label label;
+
+	private java.lang.Thread thread = getThreadByName("JavaFX Application Thread");
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String test(){
+		Platform.runLater(() -> {
+			setLabel("test");
+		});
+		return "asd";
+	}
+
+	public void setLabel(String txt){
+		this.label.setText(txt);
+	}
+
+	public java.lang.Thread getThreadByName(String threadName) {
+		for (java.lang.Thread t : java.lang.Thread.getAllStackTraces().keySet()) {
+			if (t.getName().equals(threadName)) return t;
+		}
+		return null;
+	}
+
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -34,7 +34,6 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
     }
