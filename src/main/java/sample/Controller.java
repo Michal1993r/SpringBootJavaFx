ed88060991c427fc69835f9e4d22b44b1d81b679
Michal1993r <chudychudy01>
2017-01-15 11:28:38 +0100

    rdy

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,24 +1,54 @@
 package sample;
 
-import javafx.application.Platform;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
-@org.springframework.stereotype.Controller
+import java.io.IOException;
+
+@RestController
 public class Controller {
 
-	@FXML
-	private Label label;
+    @Autowired
+    Pi4jTest test;
 
-	@RequestMapping("/")
-	@ResponseBody
-	public void test(
-			@RequestParam(name = "testParam", required = false) String param) {
-		Platform.runLater(() -> label.setText(param.equals("1") ? "test" : ""));
-	}
+    @RequestMapping("/")
+    public void direction(@RequestParam(name = "direction", required = false) String param) {
+        try {
+            System.out.println(param + "\n");
+            switch (param) {
+                case "forward":
+                    test.getBluetooth().write('w');
+                    break;
+                case "reverse":
+                    test.getBluetooth().write('s');
+                    break;
+                case "left":
+                    test.getBluetooth().write('a');
+                    break;
+                case "right":
+                    test.getBluetooth().write('d');
+                    break;
+                case "stop":
+                    test.getBluetooth().write('h');
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequestMapping("/speed")
+    public void speedChange(@RequestParam(name = "value") String value) {
+        try {
+            System.out.println("New Speed value " + value + "%\n");
+            test.getBluetooth().write('*');
+            test.getBluetooth().write(Integer.valueOf(value).byteValue());
+            test.getBluetooth().write('#');
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,40 +1,13 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan
-public class Main extends Application {
-
-    private ConfigurableApplicationContext applicationContext;
-    private Parent rootNode;
-
-    @Override
-    public void init() throws Exception {
-        applicationContext = SpringApplication.run(Main.class);
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/sample.fxml"));
-        fxmlLoader.setControllerFactory(applicationContext::getBean);
-        rootNode = fxmlLoader.load();
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(rootNode, 300, 275));
-        primaryStage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        SpringApplication.run(Main.class, args);
     }
 }
